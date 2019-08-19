5c349afa5f18b09d9cd0ff7f47dedc3f080bacc2
wk-j <somnuk.wk@gmail.com>
2019-08-19 23:16:35 +0700

    Added button

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,25 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: EdgeInsets.only(top: 8),
+          child: Text(label,
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                color: color,
+              )),
+        )
+      ],
+    );
+  }
+
   Widget titleSection = Container(
     padding: const EdgeInsets.all(32),
     child: Row(
@@ -32,6 +51,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          _buildButtonColumn(color, Icons.call, "CaLL"),
+          _buildButtonColumn(color, Icons.near_me, "ROUTE"),
+          _buildButtonColumn(color, Icons.share, "SHARE"),
+        ],
+      ),
+    );
+
     return MaterialApp(
         title: 'Flutter layout',
         theme: ThemeData(
@@ -41,7 +73,12 @@ class MyApp extends StatelessWidget {
           appBar: AppBar(
             title: Text("Flutter layout"),
           ),
-          body: Column(children: [titleSection]),
+          body: Column(
+            children: [
+              titleSection,
+              buttonSection,
+            ],
+          ),
         ));
   }
 }
